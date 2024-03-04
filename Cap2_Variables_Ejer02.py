47dffff7a81c5e10633e4be64d3127d1c907d346
LuisRuvalcba <162078206+LuisRuvalcba@users.noreply.github.com>
2024-03-03 23:03:45 -0600

    correcciones de formatos

--- Cap2_Variables_Ejer02.py
+++ Cap2_Variables_Ejer02.py
@@ -1,4 +1,2 @@
-Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> num1 = 22
-... 
+numero1 = 22
+numero2 = 8
